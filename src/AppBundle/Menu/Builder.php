f54d56a5056f135fab7809cb8aaf20c954dbb8d4
William del Rosario <williamdelrosario@yahoo.com>
2018-02-03 05:15:22 +0100

    Installed a 3rd party bundle, knplabs menu bundle

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace AppBundle\Menu;
+
+
+use Knp\Menu\MenuFactory;
+
+class Builder
+{
+    public function mainMenu(MenuFactory $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->addChild('Home', ['route' => 'homepage']);
+        $menu->addChild('Offer', ['route' => 'offer']);
+        return $menu;
+    }
+
+}
