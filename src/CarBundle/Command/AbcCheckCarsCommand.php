7ce500f7b99a0673ef0faebb91dbb5e4fb61dfd3
William del Rosario <williamdelrosario@yahoo.com>
2018-02-04 21:37:28 +0100

    Display progress of the command.

--- src/CarBundle/Command/AbcCheckCarsCommand.php
+++ src/CarBundle/Command/AbcCheckCarsCommand.php
@@ -2,12 +2,15 @@
 
 namespace CarBundle\Command;
 
+use CarBundle\Service\DataChecker;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-
 class AbcCheckCarsCommand extends ContainerAwareCommand
 {
     protected function configure()
@@ -15,7 +18,7 @@ class AbcCheckCarsCommand extends ContainerAwareCommand
         $this
             ->setName('abc:check-cars')
             ->setDescription('...')
-            ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
+            ->addArgument('format', InputArgument::OPTIONAL, 'Progress Format')
             ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
@@ -23,12 +26,21 @@ class AbcCheckCarsCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $manager = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $dataChecker = $this->getContainer()->get('car.data_checker');
         $carRepository = $manager->getRepository('CarBundle:Car');
         $cars = $carRepository->findAll();
+        $bar = new ProgressBar($output, count($cars));
 
+        $argument = $input->getArgument('format');
+        $bar->setFormat($argument);
+
+        $bar->start();
         foreach($cars as $car){
-            $output->writeln($car->getId());
+            $dataChecker->checkCar($car);
+            sleep(1);
+            $bar->advance();
         }
+        $bar->finish();
     }
 
 }
