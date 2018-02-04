e3935677d8c1cbcb9a03d2621028593c24a80361
William del Rosario <williamdelrosario@yahoo.com>
2018-02-04 16:47:52 +0100

    Passed configuration parameters to the service

--- src/CarBundle/Controller/CarController.php
+++ src/CarBundle/Controller/CarController.php
@@ -50,7 +50,11 @@ class CarController extends Controller
         $car = $em->getRepository('CarBundle:Car')->find($id);
 
         $result = $dataChecker->checkCar($car);
-        $this->addFlash('success', $result);
+        if ($result) {
+            $this->addFlash('success', 'Car promoted');
+        } else {
+            $this->addFlash('warning', 'Car not applicable');
+        }
         return $this->redirectToRoute("car_index");
     }
 
